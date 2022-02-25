96249770d8043e9d9b0ed69f914d3092a6aab231
mavico <avicomariano@gmail.com>
2022-02-25 11:34:30 -0300

    One way data binding

---
+++ 01-bases/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
